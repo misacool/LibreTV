e8d4f818faf54d2bcea78cc5e531928c73f0bb8d
misacool <yeshanghan@gmail.com>
2025-05-22 15:33:02 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -106,8 +106,8 @@ const API_SITES = {
         name: '旺旺短剧'
     },
     ikun: {
-        api: 'https://ikunzyapi.com/api.php/provide/vod',
-        name: 'iKun资源'
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源'
     },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
@@ -123,8 +123,8 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     // ckzy: {
-    //     api: 'https://www.ckzy1.com',
-    //     name: 'CK资源',
+    //     api: 'https://ikunzyapi.com/api.php/provide/vod',
+    //     name: 'iKun资源',
     //     adult: false
     // },
     // jkun: {
